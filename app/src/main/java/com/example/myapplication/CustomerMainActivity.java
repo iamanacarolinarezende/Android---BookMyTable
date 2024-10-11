46442b520cce2f96b89ad0e3ef1847e6f2525d59
Diego Ferreira <dlins_ferreira@hotmail.com>
2024-10-11 13:26:40 -0400

    Refactory second try - activity_main to activity_client_main.

--- app/src/main/java/com/example/myapplication/CustomerMainActivity.java
+++ app/src/main/java/com/example/myapplication/CustomerMainActivity.java
@@ -1,18 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class CustomerMainActivity extends AppCompatActivity {
 
@@ -23,7 +14,7 @@ public class CustomerMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_client_main);
 
 
 
--- app/src/main/java/com/example/myapplication/RestaurantMainActivity.java
+++ app/src/main/java/com/example/myapplication/RestaurantMainActivity.java
@@ -5,9 +5,6 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,7 +18,7 @@ public class RestaurantMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_client_main);
 
         showEmail = findViewById(R.id.showEmail);
         firebaseAuth = FirebaseAuth.getInstance();
